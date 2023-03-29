e7caea7de4d94da8e8492ccd694ec00f45b1b02c
Elian Ortega <elian.mortega@gmail.com>
2023-03-29 09:55:40 -0600

    chore: create todos_repository package

---
+++ graphql_todo_app/packages/todos_repository/lib/src/todos_repository.dart
@@ -0,0 +1,7 @@
+/// {@template todos_repository}
+/// A Very Good Project created by Very Good CLI.
+/// {@endtemplate}
+class TodosRepository {
+  /// {@macro todos_repository}
+  const TodosRepository();
+}
---
+++ graphql_todo_app/packages/todos_repository/lib/todos_repository.dart
@@ -0,0 +1,4 @@
+/// A Very Good Project created by Very Good CLI.
+library todos_repository;
+
+export 'src/todos_repository.dart';
